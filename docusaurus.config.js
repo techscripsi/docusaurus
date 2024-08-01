537178d2d929b9a5fd1a8ae5a952e1090b40e977
a.e.stepanov <a.e.stepanov@vk.team>
2024-08-02 01:22:09 +0300

    fix workflow attempt

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "QPaymets",
+  title: "QPayments",
   tagline: " ",
   url: "https://techscripsi.github.io/docusaurus/",
   baseUrl: "/docusaurus/",
