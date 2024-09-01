4bd0812edc013ecd8350477ca994d1c047fde612
a.e.stepanov <a.e.stepanov@vk.team>
2024-09-02 02:33:51 +0300

    svg exp

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'QPay',
   tagline: ' ',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
   url: 'https://techscripsi.github.io/',
